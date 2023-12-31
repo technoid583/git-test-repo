75475f46a378b5f9999c2ad1c9d6c6be603e2036
technoid583 <technoid583@gmail.com>
2023-12-31 22:06:13 +0500

    added a new feature-e

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ console.log("feature-c")
 
 //git feature-d branch
 console.log("feature-d")
+
+// let's say its a new feature
+let newFeature = 'feature-e';
+
+alert("It's an new feature called: " + newFeature);
